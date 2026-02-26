a9a4cba2ceb0b7210826ef479c801adcf9c0a7a7
Ghofran Mohammed <your_github_GhufranAsiri@outlook.com>
2026-02-26 03:55:00 +0300

    Update custom user fields and admin

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {
+            "fields": ("phone", "university_id", "bio"),
+        }),
+    )
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=15, blank=True, null=True)
     university_id = models.CharField(max_length=20, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.username
