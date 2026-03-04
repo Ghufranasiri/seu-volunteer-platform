76abd810c9f50a51404dc340debc5460d0529a1d
Maryam Abdulhamid <mem@Maryams-MacBook-Air.local>
2026-03-04 07:53:22 +0300

    registered application model in admin interface

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Application
 
-# Register your models here.
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('student', 'opportunity', 'status', 'applied_at') # الأعمدة التي ستظهر للمشرف
+    list_filter = ('status', 'applied_at') # فلاتر جانبية للبحث
+    search_fields = ('student__username', 'opportunity__title') # إمكانية البحث باسم الطالب أو الفرصة
+    list_editable = ('status',) # تمكين المشرف من تغيير الحالة مباشرة من القائمة
+    
