8533be033b652434870b564d861923ac342d0f63
Ghofran Mohammed <your_github_GhufranAsiri@outlook.com>
2026-03-05 02:51:37 +0300

    Add role field to CustomUser

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=15, blank=True, null=True)
     university_id = models.CharField(max_length=20, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.username
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('supervisor', 'Supervisor'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
