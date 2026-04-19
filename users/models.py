604d7fca0503e28cd90afeea7f326ed9e00ea61d
Ghofran Mohammed <your_github_GhufranAsiri@outlook.com>
2026-04-20 00:20:27 +0300

    save current work before certificates feature

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=15, blank=True, null=True)
     university_id = models.CharField(max_length=20, blank=True, null=True)
