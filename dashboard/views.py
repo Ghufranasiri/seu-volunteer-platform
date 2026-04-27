ad9005a2be23ce942d01146f5625564f86943919
derunally <detectiverh53@gmail.com>
2026-04-27 14:38:33 +0300

    Added simple AI recommendation feature in dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
+from .ai_recommendation import recommend_opportunities
 
 def dashboard_view(request):
+    # Example user data 
+    user = {
+    "major": "Education",
+    "interests": ["teaching", "technology"]
+}
+
+    # Temporary opportunities 
+    opportunities = []
+
+    recommended = recommend_opportunities(user, opportunities)
+
     context = {
         "total_opportunities": 10,
         "total_applications": 25,
-        "total_hours": 120
+        "total_hours": 120,
+        "recommended": recommended
     }
 
     return render(request, "dashboard/dashboard.html", context)
-from django.shortcuts import render
-
-def dashboard_view(request):
-    return render(request, "dashboard/dashboard.html")
